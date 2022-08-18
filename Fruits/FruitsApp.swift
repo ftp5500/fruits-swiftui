5d195979315af41bbaff629134e648b13ac39f9e
ftp5500 <ftp5500@yahoo.com>
2022-08-18 21:53:19 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Ali on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
